555a7f9d38abb716941cdba177a82743485495bb
despinosa <despinosa@inmotionsoftware.com>
2019-08-22 11:47:53 -0500

    Setup collection view cell and view controller

---
+++ Trendy/Controller/AlbumCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumCollectionViewCell.swift
+//  Trendy
+//
+//  Created by Diego Espinosa on 8/22/19.
+//  Copyright © 2019 Diego Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumArtImageView: UIImageView!
+    @IBOutlet weak var albumTitleLabel: UILabel!
+    
+}
---
+++ Trendy/Controller/AlbumCollectionViewController.swift
@@ -0,0 +1,92 @@
+//
+//  AlbumCollectionViewController.swift
+//  Trendy
+//
+//  Created by Diego Espinosa on 8/22/19.
+//  Copyright © 2019 Diego Espinosa. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "albumCell"
+private let trendingAlbumsUrl = "theaudiodb.com/api/v1/json/1/trending.php?country=us&type=itunes&format=albums"
+
+class AlbumCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        //self.collectionView!.register(AlbumCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> AlbumCollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! AlbumCollectionViewCell
+    
+        // Configure the cell
+        //cell.albumArtImageView.image = //"https://www.theaudiodb.com/images/media/album/thumb/xtvvsp1565349442.jpg"
+        //cell.albumTitleLabel.text = "Port of Miami 2"
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
