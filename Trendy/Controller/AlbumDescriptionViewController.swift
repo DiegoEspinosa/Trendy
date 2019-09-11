cd97b3c6115e48d13db00a8aeee6b80e0b6b24f5
despinosa <despinosa@inmotionsoftware.com>
2019-09-11 15:19:41 -0500

    Added new view controller in storyboard and added AlbumDescriptionViewController swift file

---
+++ Trendy/Controller/AlbumDescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumDescriptionViewController.swift
+//  Trendy
+//
+//  Created by Diego Espinosa on 9/11/19.
+//  Copyright © 2019 Diego Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class AlbumDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
